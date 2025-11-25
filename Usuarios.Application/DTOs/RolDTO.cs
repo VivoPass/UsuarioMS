63f439255830bda82d87c0af00cceba2114199cc
mariibarra14 <mariangelicaibarraluces@gmail.com>
2025-11-25 15:18:04 -0400

    FIX documentacion
    Arreglo de la documentacion del MS

--- Usuarios.Application/DTOs/RolDTO.cs
+++ Usuarios.Application/DTOs/RolDTO.cs
@@ -1,10 +1,24 @@
 ﻿
 namespace Usuarios.Application.DTOs
 {
+    /// <summary>
+    /// DTO que representa la información de un Rol dentro del sistema.
+    /// </summary>
     public class RolDTO
     {
+        /// <summary>
+        /// ID único interno del Rol en la base de datos del microservicio de Usuarios.
+        /// </summary>
         public string IdRol { get; set; }
+
+        /// <summary>
+        /// Nombre descriptivo del Rol (ej. "Administrador", "Cliente").
+        /// </summary>
         public string NombreRol { get; set; }
+
+        /// <summary>
+        /// ID del Rol asociado en el sistema de gestión de identidades externo (Keycloak).
+        /// </summary>
         public string IdRolKeycloak { get; set; }
     }
 }
